e4511adb125ff2cdfafe7b2ff29f1ebb83ac4973
William Lohrmann <wlohrmann@oni.bio>
2020-09-15 23:10:38 +0100

    Remove unused

--- src/models.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Queryable)]
-pub struct Recipe {
-    author: String,
-    title: String,
-    source: String,
-    instructions: String,
-    ingredients: String,
-    date_added: String
-}
